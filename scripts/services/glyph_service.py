2b325170c46b65bb8ffb203019708912e0946db0
TakWolf <takwolf@foxmail.com>
2024-05-23 23:31:20 +0800

    使用 pathlib 替换 os.path

--- scripts/services/glyph_service.py
+++ scripts/services/glyph_service.py
@@ -1,5 +1,4 @@
 import logging
-import os.path
 
 from scripts.configs import path_define
 from scripts.utils import bitmap_util
@@ -10,7 +9,7 @@ logger = logging.getLogger('glyph_service')
 def load_fragments(font_size: int) -> dict[int, list[list[int]]]:
     fragments = {}
     for i in range(1, 9):
-        file_path = os.path.join(path_define.fragments_dir, str(font_size), f'{i}.png')
+        file_path = path_define.fragments_dir.joinpath(str(font_size), f'{i}.png')
         data, width, height = bitmap_util.load_png(file_path)
         assert width == font_size // 2
         assert height == font_size
@@ -20,8 +19,8 @@ def load_fragments(font_size: int) -> dict[int, list[list[int]]]:
 
 
 def make_patterns(font_size: int, fragments: dict[int, list[list[int]]]):
-    outputs_dir = os.path.join(path_define.outputs_dir, str(font_size), '2800-28FF Braille Patterns')
-    os.makedirs(outputs_dir, exist_ok=True)
+    outputs_dir = path_define.outputs_dir.joinpath(str(font_size), '2800-28FF Braille Patterns')
+    outputs_dir.mkdir(parents=True, exist_ok=True)
 
     for code_point in range(0x2800, 0x28FF + 1):
         data = [[0] * (font_size // 2) for _ in range(font_size)]
@@ -35,6 +34,6 @@ def make_patterns(font_size: int, fragments: dict[int, list[list[int]]]):
                     if alpha == 0:
                         continue
                     data[y][x] = 1
-        file_path = os.path.join(outputs_dir, f'{code_point:04X}.png')
+        file_path = outputs_dir.joinpath(f'{code_point:04X}.png')
         bitmap_util.save_png(data, file_path)
         logger.info("Make pattern file: '%s'", file_path)
