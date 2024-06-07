09a075d009d5dd715fc4ef0e22374530dc42b610
TakWolf <takwolf@foxmail.com>
2024-06-07 10:08:45 +0800

    命名调整

--- scripts/services/glyph_service.py
+++ scripts/services/glyph_service.py
@@ -10,11 +10,11 @@ def load_fragments(font_size: int) -> dict[int, list[list[int]]]:
     fragments = {}
     for i in range(1, 9):
         file_path = path_define.fragments_dir.joinpath(str(font_size), f'{i}.png')
-        data, width, height = bitmap_util.load_png(file_path)
+        bitmap, width, height = bitmap_util.load_png(file_path)
         assert width == font_size // 2
         assert height == font_size
-        bitmap_util.save_png(data, file_path)
-        fragments[i] = data
+        bitmap_util.save_png(bitmap, file_path)
+        fragments[i] = bitmap
     return fragments
 
 
@@ -23,7 +23,7 @@ def make_patterns(font_size: int, fragments: dict[int, list[list[int]]]):
     outputs_dir.mkdir(parents=True, exist_ok=True)
 
     for code_point in range(0x2800, 0x28FF + 1):
-        data = [[0] * (font_size // 2) for _ in range(font_size)]
+        bitmap = [[0] * (font_size // 2) for _ in range(font_size)]
         bin_string = f'{code_point - 0x2800:08b}'
         for i, c in enumerate(reversed(bin_string)):
             if c == '0':
@@ -33,7 +33,7 @@ def make_patterns(font_size: int, fragments: dict[int, list[list[int]]]):
                 for x, alpha in enumerate(fragment_row):
                     if alpha == 0:
                         continue
-                    data[y][x] = 1
+                    bitmap[y][x] = 1
         file_path = outputs_dir.joinpath(f'{code_point:04X}.png')
-        bitmap_util.save_png(data, file_path)
+        bitmap_util.save_png(bitmap, file_path)
         logger.info("Make pattern: '%s'", file_path)
